04fe2abf580763ac91d8f48f22547a10871f1a1c
Rory Standley <rorystandley@gmail.com>
2018-10-22 20:56:00 +0100

    Adding fixture interface to fixture component

--- src/components/fixture/fixture.ts
+++ src/components/fixture/fixture.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Fixture } from "../../models/fixture";
 
 @Component( {
 	selector: 'app-fixture',
@@ -6,7 +7,7 @@ import { Component, Input } from '@angular/core';
 } )
 export class FixtureComponent {
 
-	@Input() data: any;
+	@Input() data: Fixture;
 
 	constructor() {
 	}
