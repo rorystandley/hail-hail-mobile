461c9d6556b1076b1ec7e1a068bd3cb93a0300cc
Rory Standley <rorystandley@gmail.com>
2018-11-10 23:42:11 +0000

    Implementing the player article into the team page

--- src/components/player-article/player-article.ts
+++ src/components/player-article/player-article.ts
@@ -1,15 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PlayerArticle } from "../../models/player-article";
+import { PlayerPage } from "../../pages/player/player";
+import { App } from "ionic-angular";
 
 @Component( {
 	selector: 'app-player-article',
 	templateUrl: 'player-article.html'
 } )
-export class PlayerArticleComponent {
+export class PlayerArticleComponent implements OnInit {
 
-	@Input() data: PlayerArticle
+	@Input() data: PlayerArticle;
+	nav: any;
 
-	constructor() {
+	constructor( public _app: App ) {
+	}
+
+	ngOnInit() {
+		/**
+		 * @type {NavControllerBase}
+		 */
+		this.nav = this._app.getActiveNavs()[ 0 ];
+	}
+
+	/**
+	 * Link through to the single player-article page with id provided
+	 * @param id
+	 */
+	pushPage( id ) {
+		this.nav.push( PlayerPage, { id: id } );
 	}
 
 }
